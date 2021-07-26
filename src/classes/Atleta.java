49f55b5be8613cbf9d069cf4094785d1816c05e0
codeYann <codeyan10@gmail.com>
2021-07-26 20:25:53 -0300

    Implementação do adicionar uma medalha no arraylist + Método para listar e contar quantas medalhas de um determinado tipo o atleta possui

--- src/classes/Atleta.java
+++ src/classes/Atleta.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import interfaces.OperacoesComuns;
 import lib.Utils;
 
-public class Atleta implements OperacoesComuns{
+public class Atleta implements OperacoesComuns {
 	
 	private String nome;
 	private String sexo;
@@ -34,6 +34,22 @@ public class Atleta implements OperacoesComuns{
 	public void setMedalhas(ArrayList<Medalha> medalhas) {
 		this.medalhas = medalhas;
 	}
+	
+	public void adicionarMedalhasDoAtleta() {
+		Medalha medalha = new Medalha();
+		String strMedalha;
+		int ano;
+	
+		System.out.printf("Digite a medalha do atleta: ");
+		strMedalha = Utils.entrada.nextLine();
+		medalha.setMaterial(strMedalha);
+
+		System.out.printf("Digite o ano de aquisição da medalha: ");
+		ano = Utils.entrada.nextInt();
+		medalha.setAno(ano);
+
+		this.medalhas.add(medalha);
+	}
 
 	public Medalha removerMedalha(int ano) {
 		Medalha medalhaRemovida = null;
@@ -44,10 +60,20 @@ public class Atleta implements OperacoesComuns{
 		}
 		return medalhaRemovida;
 	} 
+	
+	public void listarMedalhasAtleta(String tipo) {
+		int count = 0;
+		for(int i = 0; i < this.medalhas.size(); i++) {
+			if(tipo == this.medalhas.get(i).getMaterial()) {
+				System.out.printf("Medalha: %s, ano: %d\n", this.medalhas.get(i).getMaterial(), this.medalhas.get(i).getAno());
+				count++;
+			}
+		}
+		System.out.printf("Quantidade de medalhas de %s ganhas pelo atetla: %d\n", tipo, count);
+	}
 
 	@Override
 	public void adicionar() {
-		
 		String temporario;
 		
 		System.out.printf("Digite o nome do Atleta: ");
@@ -59,58 +85,27 @@ public class Atleta implements OperacoesComuns{
 			System.out.printf("\nDeseja adicionar Medalhas agora? digite... 'sim' ou 'não' caso contrário: ");
 			temporario = Utils.entrada.nextLine();
 			
-			if (temporario.equalsIgnoreCase("sim"))      this.adicionarMedalhasDoAtleta();
-			else if (temporario.equalsIgnoreCase("não")) break;
-			else                                         System.out.println("Você digitou uma opção inválida! Digite novamente.");
+			if (temporario.equalsIgnoreCase("sim"))      
+				this.adicionarMedalhasDoAtleta();
+			else if (temporario.equalsIgnoreCase("não")) 
+				break;
+			else                   
+				System.out.println("Você digitou uma opção inválida! Digite novamente.");
 		}
 	}
 
 	@Override
 	public boolean remover(int indice) {
-		
 		return false;
 	}
 
 	@Override
 	public String buscar(String nome) {
-		
 		return null;
 	}
 
 	@Override
 	public void listarDados() {
-		
-	}
-	
-	public void adicionarMedalhasDoAtleta() {
-		String existeOuro, existePrata, existeBronze;
-		
-		System.out.printf("Atleta possui medalha de Ouro ? [sim ou não]");
-		existeOuro = Utils.entrada.nextLine();
-		System.out.printf("Atleta possui medalha de Prata ? [sim ou não]");
-		existePrata = Utils.entrada.nextLine();
-		System.out.printf("Atleta possui medalha de Bronze ? [sim ou não]");
-		existeBronze = Utils.entrada.nextLine();
-		
-		if (existeOuro.equalsIgnoreCase("sim")) {
-			Medalha ouro = new Medalha();
-			ouro.setMaterial("Ouro");
-			ouro.adicionar();
-			this.medalhas.add(ouro);
-		}
-		
-		if (existePrata.equalsIgnoreCase("sim")) {
-			Medalha prata= new Medalha();
-			prata.setMaterial("Prata");
-			prata.adicionar();
-			this.medalhas.add(prata);
-		}
-		
-		if (existeBronze.equalsIgnoreCase("sim")) {
-			Medalha bronze = new Medalha();
-			bronze.setMaterial("Bronze");
-			bronze.adicionar();
-			this.medalhas.add(bronze);
-		}
+		return;
 	}
 }
