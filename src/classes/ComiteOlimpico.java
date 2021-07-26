b511cf905c579853f37fd3503fd6445abbbc62de
paulo <pauloalencar.aluno@gmail.com>
2021-07-26 19:04:19 -0300

    +remover +listar

--- src/classes/ComiteOlimpico.java
+++ src/classes/ComiteOlimpico.java
@@ -60,7 +60,7 @@ public class ComiteOlimpico implements OperacoesComuns{
 		this.pais = Utils.entrada.nextLine();
 		
 		while (true) {
-			System.out.printf("Deseja adicionar equipe agora? digite... 'sim' ou 'não' caso contrário: ");
+			System.out.printf("\nDeseja adicionar equipe agora? digite... 'sim' ou 'não' caso contrário: ");
 			temporario = Utils.entrada.nextLine();
 			
 			if (temporario.equalsIgnoreCase("sim"))      this.adicionarEquipeAoComite();
@@ -82,7 +82,11 @@ public class ComiteOlimpico implements OperacoesComuns{
 
 	@Override
 	public void listarDados() {
-		
+		System.out.println("Nome do comitê = " + this.nome + " País do Comitê = " + this.pais);
+		System.out.println("Equipes deste comitê: ");
+		for (Equipe equipe : this.equipes) {
+			System.out.println("Equipe - nome = " + equipe.getNome() + ", " + " modalidade = " + equipe.getModalidade());
+		}
 	}
 
 	
@@ -92,4 +96,13 @@ public class ComiteOlimpico implements OperacoesComuns{
 		this.equipes.add(equipe);
 	}
 	
+	public void alterarComite(int indice) {
+		ComiteOlimpico comite = lista_comites.get(indice);
+		
+		System.out.println("Digite o novo nome do comitê: ");
+		comite.setNome(Utils.entrada.nextLine());
+		
+		System.out.println("Digite o novo país do comitê: ");
+		comite.setPais(Utils.entrada.nextLine());
+	}
 }
