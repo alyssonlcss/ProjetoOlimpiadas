1b8201cf22451c2468631c4a3dc29db813b89e41
codeYann <codeyan10@gmail.com>
2021-07-28 17:15:50 -0300

    Implementando alterar atleta

--- src/main/Main.java
+++ src/main/Main.java
@@ -135,7 +135,10 @@ public class Main {
                                     ComiteOlimpico.getLista_comites().get(result).getEquipe().get(i).removerAtletaDeEquipe(j);
                                     break;
                                 case 3:
-                                    // ALTERAR ATLETA 
+                                    result = Utils.escolhaComite();
+                                    int i = Utils.escolherEquipe(result);
+                                    int j = Utils.escolherAtleta(result, i);
+                                    ComiteOlimpico.getLista_comites().get(result).getEquipe().get(i).getAtletas().get(j).alterarAtleta();
                                     break;
                                 case 4:
                                     // LISTAR ATLETA						
