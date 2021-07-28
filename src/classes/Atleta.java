e40bc341ee277145bcc942571342a9f14e0ae7b4
codeYann <codeyan10@gmail.com>
2021-07-28 17:14:47 -0300

    Implementando o método alterar atleta, aplicando polimorfismo na classe atleta -> Estendendo da classe credenciadoOlimpico

--- src/classes/Atleta.java
+++ src/classes/Atleta.java
@@ -3,87 +3,102 @@ package classes;
 import java.util.ArrayList;
 import interfaces.OperacoesComuns;
 import lib.Utils;
+import java.util.Random;
 
-public class Atleta implements OperacoesComuns {
-	
-	private String nome;
-	private String sexo;
-	private ArrayList<Integer> medalhas = new ArrayList<>();
-	
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getSexo() {
-		return sexo;
-	}
-	
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-	
-	public ArrayList<Integer> getMedalhas() {
-		return medalhas;
-	}
+public class Atleta extends CredenciadoOlimpico implements OperacoesComuns {
 
-	public void setMedalhas(ArrayList<Integer> medalhas) {
-		this.medalhas = medalhas;
-	}
+    private String nome;
+    private String sexo;
+    private int id;
+    private ArrayList<Integer> medalhas = new ArrayList<>();
 
-
-	@Override
-	public void adicionar() {
-		String temporario;
-		
-		System.out.printf("Digite o nome do Atleta: ");
-		this.nome = Utils.entrada.nextLine();
-		System.out.printf("Digite o sexo do Atleta: ");
-		this.sexo = Utils.entrada.nextLine(); 
-		
-		
-		System.out.printf("\nDeseja adicionar Medalhas agora? digite... 'sim' ou 'não' caso contrário: ");
-		temporario = Utils.entrada.nextLine(); 
-			
-		if (temporario.equalsIgnoreCase("sim")) this.adicionarMedalhasDoAtleta();
-	}
-
-	@Override
-	public void listarDados() {
-		
-	}
+    public String getSexo() {
+        return sexo;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
 	
-	public void alterarAtleta() {
-		
-	}
-	
-	public static Atleta buscar(String nome) {
+    public ArrayList<Integer> getMedalhas() {
+        return medalhas;
+    }
 
-		for (ComiteOlimpico comite : ComiteOlimpico.getLista_comites()) {
-			for (Equipe equipe : comite.getEquipe()) {
-				for (Atleta atleta : equipe.getAtletas()) {
-					if (atleta.getNome().equalsIgnoreCase(nome)) {
-						return atleta;
-					}
-				}
-			}
-		}
-		return null;
-	}
+    public void setMedalhas(ArrayList<Integer> medalhas) {
+        this.medalhas = medalhas;
+    }
 
-	public void adicionarMedalhasDoAtleta() {
-		
-		System.out.printf("Digite a quantidade da medalha de Ouro: ");
-		this.medalhas.add(Utils.entrada.nextInt());
-		
-		System.out.printf("Digite a quantidade da medalha de Prata: ");
-		this.medalhas.add(Utils.entrada.nextInt());
-		
-		System.out.printf("Digite a quantidade da medalha de Bronze: ");
-		this.medalhas.add(Utils.entrada.nextInt());	 Utils.entrada.nextLine();
-	}
+    @Override
+    public void adicionar() {
+        String temporario;
+
+        System.out.print("Digite o nome do Atleta: ");
+        super.setNome(Utils.entrada.nextLine());
+        System.out.print("Digite o sexo do Atleta: ");
+        this.setSexo(Utils.entrada.nextLine());
+        System.out.print("Digite o id do atleta: ");
+        super.setId(new Random().nextInt(30000));
+
+        System.out.print("\nDeseja adicionar Medalhas agora? digite... 'sim' ou 'não' caso contrário: ");
+        temporario = Utils.entrada.nextLine();
+
+        if (temporario.equalsIgnoreCase("sim"))
+            this.adicionarMedalhasDoAtleta();
+    }
+
+    @Override
+    public void listarDados() {
+
+    }
+
+    public void alterarAtleta() {
+        String sinalizadorNomeStr, sinalizadorSexoStr, sinalizadorMedalha;
+        System.out.print("Deseja alterar o nome do atleta? [Y/N] ");
+        sinalizadorNomeStr = Utils.entrada.nextLine().toLowerCase();
+
+        if(sinalizadorNomeStr == "y") {
+            System.out.print("Digite um novo nome: ");
+            super.setNome(Utils.entrada.nextLine());
+        } 
+
+        System.out.print("Deseja alterar o sexo do atleta? [Y/N]");
+        sinalizadorSexoStr = Utils.entrada.nextLine().toLowerCase();
+
+        if(sinalizadorSexoStr == "y") {
+            System.out.print("Digite o sexo: ");
+            this.setSexo(Utils.entrada.nextLine());
+        }
+
+        System.out.print("Deseja mudar a quantidade de medalhas? [Y/N] ");
+        sinalizadorMedalha = Utils.entrada.nextLine();
+        if (sinalizadorMedalha == "y") {
+            this.adicionarMedalhasDoAtleta();
+        }
+    }
+
+    public static Atleta buscar(String nome) {
+
+        for (ComiteOlimpico comite : ComiteOlimpico.getLista_comites()) {
+            for (Equipe equipe : comite.getEquipe()) {
+                for (Atleta atleta : equipe.getAtletas()) {
+                    if (atleta.getNome().equalsIgnoreCase(nome)) {
+                        return atleta;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public void adicionarMedalhasDoAtleta() {
+
+        System.out.print("Digite a quantidade da medalha de Ouro: ");
+        this.medalhas.add(Utils.entrada.nextInt());
+
+        System.out.print("Digite a quantidade da medalha de Prata: ");
+        this.medalhas.add(Utils.entrada.nextInt());
+
+        System.out.print("Digite a quantidade da medalha de Bronze: ");
+        this.medalhas.add(Utils.entrada.nextInt());
+        Utils.entrada.nextLine();
+    }
 }
