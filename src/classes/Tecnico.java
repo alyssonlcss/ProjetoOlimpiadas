27c02692c64aa915d1342d65e9f5b1db722e4f8c
paulo <pauloalencar.aluno@gmail.com>
2021-07-29 21:11:53 -0300

    Aplicando formatter no arquivo Tecnico

--- src/classes/Tecnico.java
+++ src/classes/Tecnico.java
@@ -3,38 +3,38 @@ package classes;
 import interfaces.OperacoesComuns;
 import lib.Utils;
 
-public class Tecnico extends CredenciadoOlimpico implements OperacoesComuns{
+public class Tecnico extends CredenciadoOlimpico implements OperacoesComuns {
 	private String nome;
 	private String cargo;
-	
+
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	public String getCargo() {
 		return cargo;
 	}
-	
+
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
 
 	@Override
 	public void adicionar() {
-		
-		System.out.printf("Digite o nome do Técnico: ");
+
+		System.out.printf("Digite o nome do Tï¿½cnico: ");
 		this.nome = Utils.entrada.nextLine();
-		System.out.printf("Digite o cargo do Técnico: ");
-		this.cargo = Utils.entrada.nextLine(); 
-		
+		System.out.printf("Digite o cargo do Tï¿½cnico: ");
+		this.cargo = Utils.entrada.nextLine();
+
 	}
-	
+
 	public static Tecnico buscar(String nome) {
-	
+
 		for (ComiteOlimpico comite : ComiteOlimpico.getLista_comites()) {
 			for (Equipe equipe : comite.getEquipe()) {
 				for (Tecnico tecnico : equipe.getComissaoTecnica()) {
@@ -51,15 +51,18 @@ public class Tecnico extends CredenciadoOlimpico implements OperacoesComuns{
 	public void listarDados() {
 		System.out.println("Tecnico { \nnome=" + this.getNome() + ", " + "\ncargo=" + this.getCargo() + "\n");
 	}
-	
+
 	public void alterarTecnico(int indice1, int indice2, int indice3) {
-		Tecnico tecnico = ComiteOlimpico.getLista_comites().get(indice1).getEquipe().get(indice2).getComissaoTecnica().get(indice3);
-		
-		System.out.println("Digite o novo nome do técnico: "); Utils.entrada.nextLine();
+		Tecnico tecnico = ComiteOlimpico.getLista_comites().get(indice1).getEquipe().get(indice2).getComissaoTecnica()
+				.get(indice3);
+
+		System.out.println("Digite o novo nome do tï¿½cnico: ");
+		Utils.entrada.nextLine();
 		tecnico.setNome(Utils.entrada.nextLine());
-		
-		System.out.println("Digite o novo cargo do técnico: "); Utils.entrada.nextLine();
+
+		System.out.println("Digite o novo cargo do tï¿½cnico: ");
+		Utils.entrada.nextLine();
 		tecnico.setCargo(Utils.entrada.nextLine());
 	}
-	
+
 }
